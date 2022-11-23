3be217552d8c101354b74ecbbf304f224f20d308
SweetsGuy <5392176+sweetsguy@user.noreply.gitee.com>
2022-11-23 11:36:46 +0800

    1. 新增网关服务模块

---
+++ gulimall-gateway/src/main/java/com.tangmx.gulimall.gateway/GulimallGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.tangmx.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Thomson
+ * @description:
+ * @date 2022/11/23 10:50
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+}
